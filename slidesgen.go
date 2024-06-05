f02dce6802755858470d333df4f949b67cd9249b
张生 <junqiang.zhang@junqiangzhang.local>
2024-06-05 18:44:02 +0800

    add images

--- slidesgen.go
+++ slidesgen.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 )
 
-//go:embed index.html right.html slides.html
+//go:embed index.html right.html slides.html img
 var staticFS embed.FS
 
 func main() {
@@ -19,7 +19,35 @@ func main() {
 	port := flag.String("port", "8000", "服务启动端口")
 	dir := flag.String("dir", ".", "指定目录")
 	flag.Parse()
-	// 注册静态文件处理器
+	// 处理特殊内置静态文件
+
+	var fileNames = []string{
+		"blackboard.png",
+		"boardmarker-green.png",
+		"boardmarker-red.png",
+		"chalk-green.png",
+		"chalk-red.png",
+		"sponge.png",
+		"boardmarker-black.png",
+		"boardmarker-orange.png",
+		"boardmarker-yellow.png",
+		"chalk-orange.png",
+		"chalk-white.png",
+		"whiteboard.png",
+		"boardmarker-blue.png",
+		"boardmarker-purple.png",
+		"chalk-blue.png",
+		"chalk-purple.png",
+		"chalk-yellow.png",
+	}
+
+	for _, fn := range fileNames {
+		fileName := fn
+		http.HandleFunc(fmt.Sprintf("/img/%s", fileName), func(w http.ResponseWriter, r *http.Request) {
+			c, _ := staticFS.ReadFile(fmt.Sprintf("img/%s", fileName))
+			w.Write(c)
+		})
+	}
 
 	// 当文件不存在时，读取指定目录下的的相应文件
 
